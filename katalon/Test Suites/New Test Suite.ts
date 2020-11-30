a152b77c94c5c18215a7e09b9c109796bbe41930
rmnscb <rmnscb@gmail.com>
2020-11-30 22:07:50 +0000

    katalon updates

---
+++ katalon/Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ad127cad-0849-4482-ae31-cc2d2092775c</testSuiteGuid>
+   <testCaseLink>
+      <guid>da1d1e74-0601-4a88-b0f4-f5708e6526f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
